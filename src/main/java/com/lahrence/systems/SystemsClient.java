00863000fa0d38b1913e5e8457734eedd812d692
lahrence <64618724+lahrence@users.noreply.github.com>
2024-10-02 00:23:22 -0700

    Initial Commit

---
+++ src/main/java/com/lahrence/systems/SystemsClient.java
@@ -0,0 +1,10 @@
+package com.lahrence.systems;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SystemsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
--- src/main/java/com/lahrence/systems/mixin/ExampleMixin.java
+++ src/main/java/com/lahrence/systems/mixin/ExampleMixin.java
@@ -1,10 +1,11 @@
-package com.lahrence.create.systems.mixin;
+package com.lahrence.systems.mixin;
 
-import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.server.MinecraftServer;
 
 @Mixin(MinecraftServer.class)
 public class ExampleMixin {
